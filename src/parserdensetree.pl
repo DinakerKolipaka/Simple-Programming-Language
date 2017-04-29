9a902de9a06f1adbf87fc0d665cac9d69ee61182
DinakerKolipaka <dinakerkolipaka@gmail.com>
2017-04-29 16:40:09 -0700

    Changed Parser for Keywords

--- src/parserdensetree.pl
+++ src/parserdensetree.pl
@@ -1,32 +1,35 @@
-program(program(P))-->statement_list(P).
+keywords(X):-X=[if,then,else,while,true,false].
+program(program(P))-->statement_list(P),!.
 
-statement_list('statement_List'((S,L)))-->statement(S),[';'],statement_list(L),!.
-statement_list('statement_List'(S))-->statement(S),[';'].
+statement_list('statement_List'(S,L))-->statement(S),[';'],statement_list(L),!.
+statement_list('statement_List'(S))-->statement(S),[';'],!.
 
-statement(statement(S))-->assignment(S).
-statement(statement(D))-->declaration(D).
-statement(statement(P))-->print_statement(P).
-statement(statement(I))-->if_statement(I).
-statement(statement(W))-->while_statement(W).
+statement(statement(S))-->assignment(S),!.
+statement(statement(D))-->declaration(D),!.
+statement(statement(P))-->print_statement(P),!.
+statement(statement(I))-->if_statement(I),!.
+statement(statement(W))-->while_statement(W),!.
 
-assignment(assign(L,R))-->identifier(L),['='],righthand(R).
-assignment(assign(L,R))-->declaration(L),['='],righthand(R).
+assignment(assign(L,R))-->identifier(L),['='],righthand(R),!.
 righthand(R)-->comparision(R).
 righthand(R)-->expression(R).
 righthand(R)-->boolean(R).
 
+declaration(declare('int'((ID,T))))--> ['int'], identifier(ID),['='],term(T),!.
+declaration(declare('bool'(ID,T)))--> ['bool'], identifier(ID),['='],term(T),!.
 declaration(declare('int'(ID)))--> ['int'], identifier(ID).
 declaration(declare('bool'(ID)))--> ['bool'], identifier(ID).
+
 
 boolean(true)-->[true].
 boolean(false)-->[false].
 
-comparision(compareequals(E1,E2))-->expression(E1),['=='],expression(E2).
-comparision(comparegreaterequal(E1,E2))-->expression(E1),['>='],expression(E2).
-comparision(comparelesserequal(E1,E2))-->expression(E1),['<='],expression(E2).
-comparision(comparelesser(E1,E2))-->expression(E1),['<'],expression(E2).
-comparision(comparegreater(E1,E2))-->expression(E1),['>'],expression(E2).
-comparision(comparenotequal(E1,E2))-->expression(E1),['!='],expression(E2).
+comparision(compareEquals(E1,E2))-->expression(E1),['=='],expression(E2),!.
+comparision(compareGreaterEqual(E1,E2))-->expression(E1),['>='],expression(E2).
+comparision(compareLesserEqual(E1,E2))-->expression(E1), ['<='],expression(E2).
+comparision(compareGreater(E1,E2))-->expression(E1),['>'],expression(E2),!.
+comparision(compareLesser(E1,E2))-->expression(E1),['<'],expression(E2),!.
+comparision(compareNotEqual(E1,E2))-->expression(E1),['!='],expression(E2),!.
 
 expression(expression('add'(T,E)))-->term(T),['+'],expression(E),!.
 expression(expression('subtract'(T,E)))-->term(T),['-'],expression(E),!.
@@ -36,12 +39,22 @@ expression(expression('modulo'(T,E)))-->term(T),['%'],expression(E),!.
 expression(expression(T))-->term(T).
 
 
-identifier('id'(S))-->[S].
+term(term(T))-->boolean(T),!.
+term(term(T))-->identifier(T),!.
+term(term(T))-->terminal(T).
 
-term(term(T))-->terminal(T),!.
+toatom(Y,X):-term_string(X,Y).
 terminal(et(N))-->number(N).
-term(term(T))-->identifier(T).
 
+
+%identifier('id'(S))-->[S], !,{\+iskey(S)},!.
+identifier('id'(S))-->[S],{toatom(S,T),iskey(T)}.
+
+
+iskey(K):-keywords(X),\+member(K,X).
+iskey(K):-keywords(X),member(K,X),write('Error: Identifier cannot be a keyword'),false,!.
+
+callabort:-halt.
 number(0)-->[0].
 number(1)-->[1].
 number(2)-->[2].
@@ -55,12 +68,13 @@ number(9)-->[9].
 number('number'(N))-->[N],{isnumber(N)}.
 isnumber(N):-number(N).
 
+tonumber(X,Y):-term_to_atom(Y,X).
 condition('condition'(C))--> comparision(C).
 condition('condition'(B))--> boolean(B).
-condition('condition'(T))--> terminal(T).
+condition('condition'(T))--> term(T).
 
 
-print_statement('print'(E))-->['print'],expression(E).
+print_statement('print'(E))-->['print'],expression(E),!.
 print_statement('print'(C))-->['print'],comparision(C).
 print_statement('print'('nl'))-->['print'],['nl'].
 print_statement('print'(ID))-->['print'],identifier(ID).
@@ -71,13 +85,18 @@ isString(S)-->[S],{string(S)}.
 if_statement('if'(S,R))-->['if'], parent_start(S,R).
 parent_start(C,R)--> ['('], condition(C),[')'], then_block(R).
 parent_end(R)-->then_block(R).
-then_block('then'(B))--> ['then'], block(B).
 then_block('then'(B,E))--> ['then'], block(B),else_block(E),!.
+then_block('then'(B))--> ['then'], block(B).
 else_block('else'(B))-->['else'], block(B).
 block(S)--> ['{'],brace_end(S).
 brace_end(S)--> statement_list(S),['}'].
 
 
-while_statement('while'(B))--> ['while'], parent_while_start(B).
+while_statement('while'(B))--> ['while'], parent_while_start(B),!.
 parent_while_start(C,R)--> ['('], condition(C), parent_while_end(R).
 parent_while_end(R)--> [')'], block(R).
+
+
+tokenWrite :- program(T,['int',a,';',a,=,1,+,2,;,print,a,;,if,'(',a,==,1,')',then,'{',a,=,1,;,'}',else,'{',b,=,2,;,'}',;],R),
+              open('C:/Users/Dinaker/Desktop/parseroutput.txt', write, Stream),
+              write(Stream,T),nl(Stream),close(Stream).
