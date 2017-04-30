b659685a703b16b6ecb4c3042571b15b3079c06e
Manju Bisht <mannyaten@ser.mobile.asu.edu>
2017-04-30 13:23:42 -0700

    ENV initialized

--- src/sourceCode.pl
+++ src/sourceCode.pl
@@ -211,8 +211,8 @@ Interpreter Code
 /* high-level predicate to call the interpreter program. */
 
 
-eval_Program(T) :-  	add_to_env([[x, 5]], [y,20], Env),
-			reduce_Program(T, Env, _).
+eval_Program(T) :-  add_to_env([[]], [var,0], Env),
+		    reduce_Program(T, Env, _).
 
 
 /* update ENV(empty) for the first identifier */
@@ -228,10 +228,10 @@ add_to_env(E, [Id, Value], Env_New) :-  length(E, N),
     					delete(E, [Id, _], Env),
 	    				append(Env, [[Id,Value]], Env_New), !.
 
-/* update ENV, when an identifiers value is updated,  E1 = E0{I, V1} & {I, V2} */
+/* update ENV, when an identifier's value is updated,  E1 = E0{I, V1} & {I, V2} */
 add_to_env(E, [Id, Value], Env) :-  length(E, N),
                                     N > 0,
-    				    \+look_up(Id, Value, E),
+    				    			\+look_up(Id, Value, E),
                                     append(E, [[Id, Value]], Env), !.
 
 /* entry point*/
@@ -252,7 +252,7 @@ reduce_Statement(statement(T), Env, Env_New) :-     reduce_print(T, Env, Env_New
 
 /* assignment, update the ENV */
 reduce_assignment(assign(T, _), _, _, _) :-	integer(T),
-						write("Error: Integer value cannot be assigned to an Integer").
+						write("Error: Value cannot be assigned to an integer").
 
 reduce_assignment(assign(T, E), R, Env, Env_New) :- eval_term(T, R, 0, Env),
 						    \+ integer(R),
@@ -264,7 +264,7 @@ reduce_assignment(assign(T, E), R, Env, Env_New) :- eval_term(T, R, 0, Env),
 reduce_assignment(assign(T, _), R, Env, _) :- 	    eval_term(T, R, 0, Env),
 						    \+ integer(R),
 						    \+ look_up(R, _, Env),
-						    write("Error: Integer identifier not declared yet").
+						    write("Error: Variable not declared yet").
 							
 
 reduce_assignment(assign(T, E), R, Env, Env_NN) :-  eval_term(T, R, 0, Env),
@@ -272,7 +272,7 @@ reduce_assignment(assign(T, E), R, Env, Env_NN) :-  eval_term(T, R, 0, Env),
 						    look_up(R, Value, Env),
 						    reduce_expression(E, R2, Env, Env_NN),
 						    \+ type_match(Value, R2),
-					    	    write("Error: Data type mismatch").
+					    	    write("Error: Type mismatch").
 					    	    
 					    	    
 					    	    
@@ -282,58 +282,58 @@ type_match(V1, V2) :- \+ integer(V1), \+ integer(V2).
 
  
 
-reduce_declaration(declare(T, E), R, Env, Env_New) :- 	eval_declaration(T,R,Env,Env_N), 
-			    				!, 
-							eval_terminal(E, R2, Env_N, Env_NN),
-    							add_to_env(Env_NN, [R, R2], Env_New), !.
+reduce_declaration(declare(T, E), R, Env, Env_New) :- 	eval_declaration(T, R, Env, Env_N), 
+			    										!, 
+														eval_terminal(E, R2, Env_N, Env_NN),
+    													add_to_env(Env_NN, [R, R2], Env_New), !.
 
-reduce_declaration(declare(T), R, Env, Env_New) :- 	eval_declaration(T,R,Env,Env_New), !.
+reduce_declaration(declare(T), R, Env, Env_New) :- 		eval_declaration(T, R, Env, Env_New), !.
 
-reduce_declaration(declare(T), _, _,_) :- 		reduce_datatype(T, T1, _, _), 
-							integer(T1),
-							write('Error: Declaration cannot take integer value'), !.
+reduce_declaration(declare(T), _, _,_) :- 				reduce_datatype(T, T1, _, _), 
+														integer(T1),
+														write('Error: Declaration cannot take integer value'), !.
 
 /*Declare without assignment operator*/                                               
-eval_declaration(T, R, Env, Env_New) :- 		reduce_datatype(T, T1,D, Env), 
-							eval_term(T1, R, 0, _),
-							\+ integer(R),
-							add_to_env(Env, [R,D], Env_New), !.
+eval_declaration(T, R, Env, Env_New) :- 				reduce_datatype(T, T1,D, Env), 
+														eval_term(T1, R, 0, _),
+														\+ integer(R),
+														add_to_env(Env, [R,D], Env_New), !.
 
 /*Error handling-Declare operator*/ 
 reduce_datatype(int(T),T, D,_):- 	D = 0.
 reduce_datatype(bool(T),T,D,_):- 	D = false.
 
 /* eval for only if-then condition */
-reduce_If_Statement(if(If, Then), Env, Env_New) :-  	eval_Condition(If, Bool, Env, Env_N),
+reduce_If_Statement(if(If, Then), Env, Env_New) :-  eval_Condition(If, Bool, Env, Env_N),
                                                 	!,
                                                 	is_true(Bool),
                                                 	reduce_Then(Then, Env_N, Env_New).
 /* eval for if condition is true */
-reduce_If_Statement(if(If, Then, _), Env, Env_New) :- 	eval_Condition(If, Bool, Env, Env_N),
+reduce_If_Statement(if(If, Then, _), Env, Env_New) :- eval_Condition(If, Bool, Env, Env_N),
                                                         !,
                                                         is_true(Bool),
                                                         reduce_Then(Then, Env_N, Env_New).
 /* eval for if condition is false */
-reduce_If_Statement(if(If, _, Else), Env, Env_New) :- 	eval_Condition(If, Bool, Env, Env_N),
+reduce_If_Statement(if(If, _, Else), Env, Env_New) :- eval_Condition(If, Bool, Env, Env_N),
                                                         !,
                                                         \+ is_true(Bool),
                                                         reduce_Else(Else, Env_N, Env_New).
 
-reduce_Then(then(T), Env, Env_New) :-          		reduce_Statement_List(T, Env, Env_New).
+reduce_Then(then(T), Env, Env_New) :-          	reduce_Statement_List(T, Env, Env_New).
 
-reduce_Else(else(T), Env, Env_New) :-          		reduce_Statement_List(T, Env, Env_New).
+reduce_Else(else(T), Env, Env_New) :-          	reduce_Statement_List(T, Env, Env_New).
 
 %while
 
 reduce_While(while(While, Then), Env, Env_NewRepeat) :- eval_Condition(While, R, Env, Env_N), 
-						   	is_true(R) , 
-						  	reduce_Statement_List(Then, Env_N, Env_New), 
-						   	reduce_While(while(While, Then), Env_New, Env_NewRepeat).
+						   								is_true(R) , 
+						   								reduce_Statement_List(Then, Env_N, Env_New), 
+						   								reduce_While(while(While, Then), Env_New, Env_NewRepeat).
 
-reduce_While(while(While, _), Env, Env_New) :-  	eval_Condition(While, R, Env, Env_New), 
-						   	\+ is_true(R).
+reduce_While(while(While, _), Env, Env_New) :-  eval_Condition(While, R, Env, Env_New), 
+						   								\+ is_true(R).
 
-reduce_print(print(T), Env, Env_New) :- 		eval_print(T, _, Env, Env_New), !.
+reduce_print(print(T), Env, Env_New) :- eval_print(T, _, Env, Env_New), !.
 
 
 /*condition for if(), various types */
@@ -350,16 +350,17 @@ eval_boolean(term(T), Value, Env, Env_New) :-  eval_term(T, Value, Env, Env_New)
     										
 												
 eval_boolean(term(T), _, Env, _) :- 	\+ eval_term(T, _, Env, _),
-					write("Error: Boolean identifier not delcared yet").
+										write("Error: Value do not exist").
 													
 
 /* Comparison operators:  '==', ‘<’, ‘>’, ‘<=’, ‘>=’, ‘!=’ */
 
-eval_compareEquals(compareEquals(T, E), R, Env, Env_New) :- 	eval_terminal(T, R, 0, Env),
+eval_compareEquals(compareEquals(T, E), R, Env, Env_New) :- eval_terminal(T, R, 0, Env),
                                                 	      	!,
                                                 	      	reduce_expression(E, R2, Env, Env_NN),
                                                 	      	add_to_env(Env_NN, [R, R2], Env_New),
-    								nl, !.
+    														write(Env_New),
+    														nl, !.
                                                 		
 eval_compareLesser('compareLesser'(E, E1), R, Env, Env_New) :- 	reduce_expression(E, R1, Env, Env_N),!,
 								reduce_expression(E1, R2, Env_N, Env_New), 
@@ -382,9 +383,9 @@ eval_compareNotEqual('compareNotEqual'(E, E1), R, Env, Env_New) :- 	reduce_expre
 									eval(R1\=R2, R), !. 
 
 
-eval_print(T, R, Env, Env_New) :- 	reduce_expression(T, R, Env, Env_New),
-				  	write(R), 
-    					nl, !.
+eval_print(T, R, Env, Env_New) :- reduce_expression(T, R, Env, Env_New),
+				  write(R), 
+    				nl, !.
 
 check(Exp):- Exp.
 eval(Exp, R):- check(Exp), R = true.
@@ -401,58 +402,60 @@ eval_expression(add(T, E), R, Env, Env_New) :-  	eval_terminal(T, R1, Env, Env_N
                                                 	R is R1 + R2.
 
 eval_expression(subtract(T, E), R, Env, Env_New) :-   	eval_terminal(T, R1, Env, Env_N),
-                                                	!,
-                                                	reduce_expression(E, R2, Env_N, Env_New),
-                                                	R is R1 - R2, !.
+                                                		!,
+                                                		reduce_expression(E, R2, Env_N, Env_New),
+                                                		R is R1 - R2, !.
 
-eval_expression(multiply(T, E), R, Env, Env_New) :- 	eval_terminal(T, R1, Env, Env_N),
+eval_expression(multiply(T, E), R, Env, Env_New) :- eval_terminal(T, R1, Env, Env_N),
                                                 	!,
                                                 	reduce_expression(E, R2, Env_N, Env_New),
                                                 	R is R1 * R2, !.
 
 
-eval_expression(divide(T, E), R, Env, Env_New) :-   	eval_terminal(T, R, Env, Env_N),
-                                                	!,
-                                                   	reduce_expression(E, R2, Env_N, Env_New),
-					    	    	\+ check_divison_by_zero(R2),	
-						    	write("Error: Division by zero"), !.
 
-eval_expression(divide(T, E), R, Env, Env_New) :-   	eval_terminal(T, R1, Env, Env_N),
-                                                	reduce_expression(E, R2, Env_N, Env_New),
-    							check_divison_by_zero(R2),	
-                                                	R is R1 / R2, !.
+eval_expression(divide(T, E), R, Env, Env_New) :-   eval_terminal(T, R1, Env, Env_N),
+                                                    reduce_expression(E, R2, Env_N, Env_New),
+    						    check_divison_by_zero(R2),	
+                                                    R is R1 / R2, !.
+
+eval_expression(divide(T, E), R, Env, Env_New) :-   eval_terminal(T, R, Env, Env_N),
+                                                    !,
+                                                    reduce_expression(E, R2, Env_N, Env_New),
+					    	    \+ check_divison_by_zero(R2),	
+						    write("Error, division by zero"), !.
 
 
 /* eval func for mod */
 eval_expression(modulo(T, E), R, Env, Env_New)  :-    	eval_terminal(T, R1, Env, Env_N),
                                                       	!,
-                                                	reduce_expression(E, R2, Env_N, Env_New),
-                                                	R is mod(R1,R2), !.
+                                                		reduce_expression(E, R2, Env_N, Env_New),
+                                                		R is mod(R1,R2), !.
 
+check_divison_by_zero(Divisor) :- Divisor =\= 0.   
 
 /* eval func for terminal */
-eval_terminal(term(T), R, Env, Env_New) :-        	eval_term(T, R, Env, Env_New).
+eval_terminal(term(T), R, Env, Env_New) :-        eval_term(T, R, Env, Env_New).
 
 eval_term(id(T), T, Env, Env) :-   integer(T).
 
 /* when term is identifier */
 eval_term(id(T), T, 0, _) :-	\+ integer(T).
 
-eval_term(id(T), R, Env, Env) :-     \+ integer(T),
+eval_term(id(T), R, Env, Env) :-      \+ integer(T),
                                      look_up(T, Value, Env),
                                      R = Value.
 
-eval_term(T, T, Env, Env) :-  	is_true(T).
-eval_term(T, T, Env, Env) :- 	\+ is_true(T).
+eval_term(T, T, Env, Env) :-  is_true(T).
+eval_term(T, T, Env, Env) :- \+ is_true(T).
 
-check_divison_by_zero(Divisor) :- Divisor =\= 0.     					
+  					
 
 
 /* look-up for variables in env */
 
 look_up(Id, Value, [H|_]) :-    look_Id(Id, Value, H).
                                
-look_up(Id, Value, [H|T]) :-    \+ look_Id(Id, _, H),
+look_up(Id, Value, [H|T]) :-    	\+ look_Id(Id, _, H),
                                 look_up(Id, Value, T).
 
 look_Id(Id, Value, [Id, Value]).
